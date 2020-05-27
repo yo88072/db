8b3101b8952bc6baba0a8f5158b5dba45939d07a
yo88072 <62133498+yo88072@users.noreply.github.com>
2020-05-27 18:29:43 +0800

    Update admin.py

--- studentsapp/admin.py
+++ studentsapp/admin.py
@@ -3,10 +3,10 @@ from studentsapp.models import student
 
 #class studentAdmin(admin.ModelAdmin):
     # 第三種方式，加入 ModelAdmin 類別，定義顯示欄位、欄位過濾資料、搜尋和排序
-	#list_display=('id','cName','cSex','cBirthday','cEmail','cPhone','cAddr')
-	#list_filter=('cName','cSex')
-	#search_fields=('cName',)
-	#ordering=('id',)
+	list_display=('id','no','companyid','num','jobname','name','company','cash')
+	
+	search_fields=('companyid',)
+	ordering=('id',)
 	
 #admin.site.register(student,studentAdmin)
 
@@ -15,6 +15,6 @@ from studentsapp.models import student
 #admin.site.register(student)	
 
 # 第二種方式，加入 ModelAdmin 類別，定義顯示欄位
-class studentAdmin(admin.ModelAdmin):
-	list_display=('id','no','companyid','num','jobname','name','company','cash')
-admin.site.register(student,studentAdmin)
+#class studentAdmin(admin.ModelAdmin):
+	#list_display=('id','no','companyid','num','jobname','name','company','cash')
+#admin.site.register(student,studentAdmin)
